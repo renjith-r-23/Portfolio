b9c6988c1899eea5e5e53a470ef13d22801ef9c5
Renjith R <145246224+renjith-r-23@users.noreply.github.com>
2023-10-18 16:38:19 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,28 +4,37 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="bg-gray-200 py-10">
-      <div className="container mx-auto text-center">
-        <h1 className="text-3xl font-semibold mb-4 text-gray-800">
-          Apps I've Built
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            Apps I've Built
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          I take pride in the digital landscapes I've helped shape. Here's a glimpse of some of the apps I've crafted with care, creativity, and a touch of innovation. Each project represents a unique challenge and an opportunity to bring ideas to life. Let's dive in and explore the results of my coding journey.
+          </p>
+        </div>
+        <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-            <a href={project.link} key={project.image} className="rounded-lg shadow-md">
-              <div className="relative">
+            <a
+              href={project.link}
+              key={project.image}
+              className="sm:w-1/2 w-100 p-4">
+              <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="object-cover object-center rounded-t-lg"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="p-5 bg-white rounded-b-lg">
-                  <h2 className="text-sm font-medium text-gray-700 mb-2">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
-                  <h1 className="text-xl font-semibold text-gray-800 mb-2">
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
                     {project.title}
                   </h1>
-                  <p className="text-gray-600">{project.description}</p>
+                  <p className="leading-relaxed">{project.description}</p>
                 </div>
               </div>
             </a>
