7198953f3099e1236e960e28bdc29f01c6c7f4f5
Renjith R <145246224+renjith-r-23@users.noreply.github.com>
2023-10-18 16:35:29 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,86 +2,96 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <section id="contact" className="bg-gray-100 py-10">
-      <div className="container mx-auto text-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="bg-white p-5 rounded-lg shadow-md">
-            <iframe
-              title="map"
-              width="100%"
-              height="300"
-              frameBorder={0}
-              marginHeight={0}
-              marginWidth={0}
-              style={{ filter: "opacity(0.7)" }}
-              src="https://www.google.com/maps/embed/v1/place?q=Pathanamthitta,+Kerala,+India&key=YOUR_API_KEY"
-            />
-            <div className="mt-4">
-              <h2 className="text-lg font-semibold text-gray-800">ADDRESS</h2>
-              <p className="text-gray-600">Pathanamtitta, 690504</p>
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed/v1/place?q=Pathanamthitta,+Kerala,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          />
+          
+        
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                Pathanamtitta <br />
+                690504
+              </p>
             </div>
-            <div className="mt-4">
-              <h2 className="text-lg font-semibold text-gray-800">EMAIL</h2>
-              <a href="mailto:renjithnrd3@gmail.com" className="text-indigo-500">
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
                 renjithnrd3@gmail.com
               </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">123-456-7890</p>
             </div>
-            <div className="mt-4">
-              <h2 className="text-lg font-semibold text-gray-800">PHONE</h2>
-              <p className="text-gray-600">xxxx-xxxx-xx</p>
-            </div>
-          </div>
-          <div className="bg-white p-5 rounded-lg shadow-md">
-            <h2 className="text-3xl font-semibold text-gray-800">Get in touch</h2>
-            <p className="text-gray-600 mb-5">
-              I'd love to hear from you! Whether you have a project in mind, want to discuss collaboration, or simply want to say hello, don't hesitate to reach out. Feel free to contact me through the form provided below.
-            </p>
-            <form
-              name="contact"
-              className="text-left"
-              // Add form fields here
-            >
-              <div className="mb-4">
-                <label htmlFor="name" className="text-sm font-semibold text-gray-800">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full py-2 px-3 rounded-lg border border-gray-300 focus:outline-none focus:border-indigo-500"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="email" className="text-sm font-semibold text-gray-800">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full py-2 px-3 rounded-lg border border-gray-300 focus:outline-none focus:border-indigo-500"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="message" className="text-sm font-semibold text-gray-800">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows="4"
-                  className="w-full py-2 px-3 rounded-lg border border-gray-300 focus:outline-none focus:border-indigo-500 resize-none"
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-200">
-                Submit
-              </button>
-            </form>
           </div>
         </div>
+        <form
+          netlify
+          name="contact"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Get in touch
+          </h2>
+          <p className="leading-relaxed mb-5">
+          I'd love to hear from you! Whether you have a project in mind, want to discuss collaboration, or simply want to say hello, don't hesitate to reach out. Feel free to contact me through the form provided below.
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
+        </form>
       </div>
     </section>
   );
