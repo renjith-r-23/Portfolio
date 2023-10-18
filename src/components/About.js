03c0e67b9e810d0fadd2765875ff76860df1ef99
Renjith R <145246224+renjith-r-23@users.noreply.github.com>
2023-10-18 16:15:58 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function About() {
   return (
-    <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+    <section id="about" className="bg-gray-100 py-10">
+      <div className="container mx-auto flex flex-col md:flex-row items-center">
+        <div className="md:w-1/2 p-5">
+    {<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Renjith R.
             <br className="hidden lg:inline-block" />I love to build amazing
             apps.
@@ -27,11 +27,11 @@ export default function About() {
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               See My Past Work
             </a>
-          </div>
+          </div>}
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="md:w-1/2 p-5">
           <img
-            className="object-cover object-center rounded"
+            className="object-cover object-center rounded-lg shadow-md"
             alt="hero"
             src="renjith1.jpg"
           />
