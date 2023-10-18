a34ae302998c5f132af6e56715781f17271422e7
Renjith R <145246224+renjith-r-23@users.noreply.github.com>
2023-10-18 16:37:31 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,31 +3,31 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="bg-blue-900 md:sticky top-0 z-10 shadow-md">
+    <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-semibold text-white mb-4 md:mb-0 text-3xl">
-          <a href="#about" className="ml-3">
-            Renjith R
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Renjith R 
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-gray-300">
-            Past Work
+          <a href="#projects" className="mr-5 hover:text-white">
+            Past Works
           </a>
-          <a href="#skills" className="mr-5 hover:text-gray-300">
+          <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-gray-300">
+          <a href="#testimonials" className="mr-5 hover:text-white">
             Testimonials
           </a>
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-2 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 text-white">
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
           Connect with me
-          <ArrowRightIcon className="w-5 h-5 ml-1" />
+          <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
     </header>
   );
-            }
+}
