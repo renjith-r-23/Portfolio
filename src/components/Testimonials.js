b22a067719b90431d4183aa3c90990215d16e951
Renjith R <145246224+renjith-r-23@users.noreply.github.com>
2023-10-18 16:18:12 +0530

    Update Testimonials.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,32 +4,29 @@ import { testimonials } from "../data";
 
 export default function Testimonials() {
   return (
-    <section id="testimonials">
-      <div className="container px-5 py-10 mx-auto text-center">
-        <UsersIcon className="w-10 inline-block mb-4" />
-        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+    <section id="testimonials" className="bg-gray-200 py-10">
+      <div className="container mx-auto text-center">
+        <h1 className="text-3xl font-semibold mb-4 text-gray-800">
           Client Testimonials
         </h1>
-        <div className="flex flex-wrap m-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {testimonials.map((testimonial) => (
-            <div className="p-4 md:w-1/2 w-full">
-              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                <TerminalIcon className="block w-8 text-gray-500 mb-4" />
-                <p className="leading-relaxed mb-6">{testimonial.quote}</p>
-                <div className="inline-flex items-center">
-                  <img
-                    alt="testimonial"
-                    src={testimonial.image}
-                    className="w-12 rounded-full flex-shrink-0 object-cover object-center"
-                  />
-                  <span className="flex-grow flex flex-col pl-4">
-                    <span className="title-font font-medium text-white">
-                      {testimonial.name}
-                    </span>
-                    <span className="text-gray-500 text-sm uppercase">
-                      {testimonial.company}
-                    </span>
+            <div className="bg-white rounded-lg p-5 shadow-md">
+              <div className="mb-4">
+                <TerminalIcon className="w-8 h-8 text-gray-500 mb-2" />
+                <p className="text-gray-600 leading-relaxed">{testimonial.quote}</p>
+              </div>
+              <div className="flex items-center">
+                <img
+                  alt="testimonial"
+                  src={testimonial.image}
+                  className="w-12 h-12 rounded-full object-cover object-center mr-3"
+                />
+                <div>
+                  <span className="text-lg font-medium text-gray-800">
+                    {testimonial.name}
                   </span>
+                  <span className="text-gray-600 text-sm">{testimonial.company}</span>
                 </div>
               </div>
             </div>
